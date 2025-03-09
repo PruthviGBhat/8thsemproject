bcf779a1088de0d69ff8ea7e7b904f16a6bed39b
Pruthvi Bhat <pruthvigbhaveri@gmail.com>
2025-03-09 19:35:17 +0530

    create listing added

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "easyestate8thsem.firebaseapp.com",
+  projectId: "easyestate8thsem",
+  storageBucket: "easyestate8thsem.firebasestorage.app",
+  messagingSenderId: "1054138555954",
+  appId: "1:1054138555954:web:318f35d13e55d71804662b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
